310a33b840c081f4c0105af2f9ff50314d6ff96b
uhyo <uhyo@uhyohyo.net>
2016-05-20 23:19:47 +0900

    Initial commit

---
+++ run-jasmine.js
@@ -0,0 +1,17 @@
+//https://github.com/bcaudan/jasmine-spec-reporter/blob/master/docs/jasmine-npm-configuration.md
+var Jasmine = require('jasmine');
+var SpecReporter = require('jasmine-spec-reporter');
+var noop = function() {};
+
+var jrunner = new Jasmine();
+jrunner.configureDefaultReporter({print: noop});    // remove default reporter logs
+jasmine.getEnv().addReporter(new SpecReporter());   // add jasmine-spec-reporter
+jrunner.loadConfig({
+    spec_dir: 'dist/test',
+    spec_files: [
+        '**/*[sS]pec.js',
+    ],
+    stopSpecOnExpectationFailure: false,
+    random: false,
+});
+jrunner.execute();
